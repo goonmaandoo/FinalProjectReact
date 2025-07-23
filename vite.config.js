5d01ef367832c1cea9413ac1ae3d36eac4eb5e79
goonmaandoo <won020738@gmail.com>
2025-07-23 17:48:02 +0900

    스프링 연결 성고

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,  // React 개발서버 포트
+    proxy: {
+      // '/api'로 시작하는 요청은 Spring 서버(8080)로 프록시
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
