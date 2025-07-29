0c89bf8eb5cbdd5db9a9a5b8487349417c252cc0
samataewoong <ltwng99@naver.com>
2025-07-29 16:45:42 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyPage from './pages/myPage/MyPage';
+import UserInfo from './pages/myPage/UserINfo';
+import './App.css';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -11,7 +14,18 @@ function App() {
       .catch(err => console.error(err));
   }, []);
 
-  return <div>{message}</div>;
+  return (
+    <>
+      <div>{message}</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/mypage" element={<MyPage />} />
+          <Route index element={<UserInfo />} />
+          <Route path="userinfo" element={<UserInfo />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
