dd39614cdc7779bb98d4055cb604ce8813a361ab
Gungogoma <sunsh45@naver.com>
2025-07-30 12:10:24 +0900

    app.jsx import 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyPage from './pages/myPage/MyPage';
 import UserInfo from './pages/myPage/UserINfo';
-import Login from "./pages/Login";
-import RegisterCheck from "./pages/RegisterCheck";
-import OwnerRegister from "./pages/OwnerRegister";
-import UserRegister from "./pages/UserRegister";
+import Login from "./pages/loginPage/Login";
+import RegisterCheck from "./pages/loginPage/RegisterCheck";
+import OwnerRegister from "./pages/loginPage/OwnerRegister";
+import UserRegister from "./pages/loginPage/UserRegister";
 
 function App() {
   const [message, setMessage] = useState('');
