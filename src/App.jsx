ba7008478eef04f3bf8f968565b6dd57761070ad
Gungogoma <sunsh45@naver.com>
2025-07-30 16:32:30 +0900

    회원가입 완료

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,9 @@ import OwnerRegister from "./pages/loginPage/OwnerRegister";
 import UserRegister from "./pages/loginPage/UserRegister";
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(res => res.text())
-      .then(data => setMessage(data))
-      .catch(err => console.error(err));
-  }, []);
 
   return (
     <div>
-      <div>{message}</div>
       <BrowserRouter>
         <Routes>
           <Route path="/mypage" element={<MyPage />} />
