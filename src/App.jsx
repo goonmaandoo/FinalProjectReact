bda05136b3e049cd9626e040a39fc8ad481dfc98
Gungogoma <sunsh45@naver.com>
2025-07-30 12:08:20 +0900

    app.jsx login route 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyPage from './pages/myPage/MyPage';
 import UserInfo from './pages/myPage/UserINfo';
+import Login from "./pages/Login";
 import RegisterCheck from "./pages/RegisterCheck";
 import OwnerRegister from "./pages/OwnerRegister";
 import UserRegister from "./pages/UserRegister";
@@ -23,6 +24,7 @@ function App() {
         <Routes>
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/mypage/userinfo" element={<UserInfo />}gi />
+          <Route path="/login" element={<Login />} />
           <Route path="/ownerusercheck" element={<RegisterCheck />} />
           <Route path="/ownerregister" element={<OwnerRegister />} />
           <Route path="/userregister" element={<UserRegister />} />
