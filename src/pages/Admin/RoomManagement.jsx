317db5c58683c063615507c82f233de4cafc0307
goonmaandoo <won020738@gmail.com>
2025-08-11 01:48:09 +0900

    공구방관리

--- src/pages/Admin/RoomManagement.jsx
+++ src/pages/Admin/RoomManagement.jsx
@@ -2,10 +2,11 @@ import styles from '../../CSS/RoomManagement.module.css';
 import { useState, useEffect } from 'react';
 
 export default function RoomManagement() {
-    const { roomData, setRoomData } = useState([]);
+    const [roomData, setRoomData] = useState([]);
+    const [activeRoomId, setActiveRoomId] = useState(null);
 
     useEffect(() => {
-        fetch('/room/adminSelectRoom')
+        fetch('http://localhost:8080/api/room/adminSelectRoom')
             .then(res => {
                 if (!res.ok) throw new Error('서버 에러');
                 return res.json();
@@ -13,27 +14,40 @@ export default function RoomManagement() {
             .then(data => setRoomData(data))
             .catch(console.error);
     }, []);
+    // useEffect(() => {
+    //     fetch('http://localhost:8080/api/room/adminSelectRoomUser/{id}')
+    //         .then(res => {
+    //             if (!res.ok) throw new Error('서버 에러');
+    //             return res.json();
+    //         })
+    //         .then(data => setRoomData(data))
+    //         .catch(console.error);
+    // }, [])
 
     return (
-
-        <div className={styles["store_box"]}>
-            <div className={styles["total"]}>
-                <div className={styles["total_third"]}>
-                    <div>{roomData.roomName}</div>
-                    <div>가게이름</div>
+        <div>
+            {roomData.map((item) => (
+                <div key={item.id} className={styles["store_box"]}>
+                    <div className={styles["total"]}>
+                        <div className={styles["total_title"]}>
+                            <div className={styles["room_name"]}>{item.roomName}</div>
+                            <div className={styles["room_store"]}>{item.storeName}</div>
+                        </div>
+                        <div className={styles["total_num_box"]}>
+                            <div className={styles["total_num"]}>공구방번호 : {item.id}</div>
+                        </div>
+                    </div>
+                    <div className={styles["total1"]}>
+                        <div>
+                            <div className={styles["room_count"]}>공구방 인원 : ?/{item.maxPeople}</div>
+                        </div>
+                        <div className={styles["report_box"]}><div className={styles["report_status"]}>{item.status}</div></div>
+                        <button onClick={() => { setActiveRoomId(prev => (prev === item.id ? null : item.id)); }}>펼치기</button>
+                    </div>
+                    
                 </div>
-                <div className={styles["total_third"]}>
-                    <div>주문번호</div>
-                    <div>주문상품</div>
-                </div>
-            </div>
-            <div className={styles["total1"]}>
-                <div>
-                    <div>주문인원 ?/?</div>
-                </div>
-                <div className={styles["report_status"]}>미응답</div>
-                <button>펼치기</button>
-            </div>
+            ))
+            }
         </div>
     )
 }
