4d1d02a0f6f6082076adf8de0601cbeaa41ed9ad
goonmaandoo <won020738@gmail.com>
2025-08-10 22:09:31 +0900

    주문관리리

--- src/pages/Admin/OrderManagement.jsx
+++ src/pages/Admin/OrderManagement.jsx
@@ -2,9 +2,8 @@ import styles from '../../CSS/StoreManagement.module.css';
 import { useState, useEffect } from 'react';
 
 export default function OrderManagement() {
-    const [storeCount, setStoreCount] = useState(0);
-    const [store, setStore] = useState([]);
-    const [selected, setSelected] = useState('all');
+    const [orders, setOrders] = useState([]);
+    const [selected, setSelected] = useState('orderId');
     const [keyword, setKeyword] = useState("");
 
     const handleChange = (e) => {
@@ -12,7 +11,8 @@ export default function OrderManagement() {
     };
 
     const handleSearch = () => {
-        let url = 'http://localhost:8080/store/search';
+        let url = 'http://localhost:8080/api/orders/orderSearch';
+        console.log(orders.map(o => o.orderId))
         if (selected !== 'all' && keyword.trim() !== '') {
             url += `?type=${selected}&keyword=${encodeURIComponent(keyword)}`;
         }
@@ -21,35 +21,28 @@ export default function OrderManagement() {
                 if (!res.ok) throw new Error('서버 에러');
                 return res.json();
             })
-            .then(data => setStore(data))
+            .then(data => {console.log(data); setOrders(data)})
             .catch(console.error);
     };
     //데이터 불러오기
     useEffect(() => {
-        fetch('http://localhost:8080/store/storeCount')
+        fetch('http://localhost:8080/api/orders/orderList')
             .then(res => {
                 if (!res.ok) throw new Error('서버 에러');
                 return res.json();
             })
-            .then(count => setStoreCount(count))
-            .catch(console.error);
-        fetch('http://localhost:8080/store/userAll')
-            .then(res => {
-                if (!res.ok) throw new Error('서버 에러');
-                return res.json();
-            })
-            .then(data => setStore(data))
+            .then(data => {console.log(data); setOrders(data)})
+            
             .catch(console.error);
     }, [])
     return (
         <>
             <div className={styles["input_value"]}>
                 <select id="table_th" value={selected} onChange={handleChange}>
-                    <option value="all">전체</option>
-                    <option value="user">사용자</option>
+                    <option value="orderId">주문번호</option>
+                    <option value="roomId">채팅방</option>
+                    <option value="nickname">사용자</option>
                     <option value="storeName">가게이름</option>
-                    <option value="storeAddress">가게주소</option>
-                    <option value="tel">전화번호</option>
                 </select>
                 <input type='text' value={keyword} onChange={(e) => setKeyword(e.target.value)} />
                 <button onClick={handleSearch}>검색</button>
@@ -58,15 +51,15 @@ export default function OrderManagement() {
             <table className={styles["store_table"]}>
                 <thead>
                     <tr>
-                        <th>주문번호</th><th>채팅방</th><th>사용자ID</th><th>가게이름</th><th>가게주문</th><th>총주문금액</th><th>주문일시시</th>
+                        <th>주문번호</th><th>채팅방</th><th>사용자ID</th><th>가게이름</th><th>총주문금액</th><th>주문일시</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* {store.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.id}</td><td>{item.nickName}</td><td>{item.storeName}</td><td>{item.storeAddress}</td><td>{item.minPrice}</td><td>{item.tel}</td>
+                    {orders.map((item) => (
+                        <tr key={item.orderId}>
+                            <td>{item.orderId}</td><td>{item.roomId}</td><td>{item.nickname}</td><td>{item.storeName}</td><td>{item.totalPrice}</td><td>{item.createdAt}</td>
                         </tr>
-                    ))} */}
+                    ))}
                 </tbody>
 
             </table>
